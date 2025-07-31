92dc97f7ed4c60cb0768c6677820669e97f1ee22
gayatri-8010 <gayatrithorat03@gmail.com>
2025-07-31 09:13:39 +0530

    code for final section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero-section/Hero";
+import WhyDukaan from "./components/why-dukaan/WhyDukaan";
+import Marquee from "./components/marquee/Marquee";
+import GrowthCard from "./components/growth-card/GrowthCard";
+import Features from "./components/growth-card/Features";
+import Themes from "./components/theme/Themes";
+import EnhancePlugins from "./components/plugins/EnhancePlugins";
+import FinalSection from "./components/FinalSection/FinalSection";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Marquee />
+      <WhyDukaan />
+      <GrowthCard/>
+       <Features />
+       <Themes />
+       <EnhancePlugins />
+       <FinalSection />
     </>
   );
 }
